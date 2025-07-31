99c2affc008ded3a128d3914d762f7a4cd8bd2f0
JimcostDev <rjak727@gmail.com>
2025-07-31 13:20:04 +0200

    add tests de go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hello-git
+
+go 1.24.1
--- hello.go
+++ hello.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
-func main(){
+// GetGreeting retorna el mensaje de saludo
+func GetGreeting() string {
+	return "Hola Mundo en Go!"
+}
 
+// PrintGreeting imprime el mensaje de saludo
+func PrintGreeting() {
+	fmt.Println(GetGreeting())
+}
+
+func main() {
 	// Hola Mundo
-	fmt.Println("Hola Mundo en Go!")
+	PrintGreeting()
 }
